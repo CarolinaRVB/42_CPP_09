112df47ee8c6278681b2be24a7a99d92e3a6ddf4
CarolinaRVB <carolinavbastos@hotmail.com>
2024-12-05 18:51:38 +0000

    pseudo-code

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 17:50:34 by crebelo-          #+#    #+#             */
+/*   Updated: 2024/12/05 18:10:10 by crebelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+/*
+    Could use a key-par value container to access the
+    bitcoin values my matching dates --> which one? map or set?
+*/
+
+#endif
+
