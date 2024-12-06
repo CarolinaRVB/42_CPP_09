4e32c95c1bf7b6c3968711016976330286b254dc
Carolina Rebelo De Vela Bastos Bastos <crebelo-@c2r8s13.42lisboa.com>
2024-12-06 15:53:01 +0000

    updates

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -11,7 +11,26 @@
 /* ************************************************************************** */
 
 # include "BitcoinExchange.hpp"
+# include <fstream>
+
+void    save_file(std::fstream &file, std::map<std::string, float> map) {
+    std::string line;
+
+    while (!file.eof()) {
+        getline(file, line);
+    }
+}
 
 int main(int argc, char **argv) {
     
+    std::fstream file;
+
+    if (argc == 1)
+        std::cout << "Error: could not open file\n";
+    file.open(argv[1], std::fstream::in);
+    if (file.fail())
+        std::cout << "Error: could not open file\n";
+
+    std::map<std::string, float>    map;
+    save_file(file, map);
 }
